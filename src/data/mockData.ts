01c8ff5bd2af6b6c97ec78b21b2e62bd3fd712c4
rossi manuel <m4nu.r0ssi@gmail.com>
2026-04-04 18:25:35 +0000

    🔄 Cloud Sync: auto - 2026-04-04 18:25

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -2,10 +2,10 @@ import { Recipe } from '../types';
     
 /**
  * Recettes synchronisées depuis WordPress
- * Dernière mise à jour: 04/04/2026 18:08:47
+ * Dernière mise à jour: 04/04/2026 18:25:35
  * Total: 178 recettes
  */
-export const exportSyncId = "1775326127049";
+export const exportSyncId = "1775327135740";
 export const mockRecipes: Recipe[] = [
     {
         "id": "4818",
