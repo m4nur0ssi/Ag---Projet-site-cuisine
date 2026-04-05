943851efac6a5cc98831c0182ad11e4d977fdd92
rossi manuel <m4nu.r0ssi@gmail.com>
2026-04-05 06:54:13 +0000

    🍳 Nouvelle recette (Cloud): Ramen de poulet

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -2,10 +2,10 @@ import { Recipe } from '../types';
     
 /**
  * Recettes synchronisées depuis WordPress
- * Dernière mise à jour: 05/04/2026 04:33:12
+ * Dernière mise à jour: 05/04/2026 06:54:13
  * Total: 179 recettes
  */
-export const exportSyncId = "1775363592656";
+export const exportSyncId = "1775372053349";
 export const mockRecipes: Recipe[] = [
     {
         "id": "4823",
